80bf68936fefd2865219dd1d29bbf6fccd0c32e8
Rana Ranvijay SIngh <ranaranvijaysingh@Ranvijay-Mac-mini.local>
2017-02-22 14:19:13 +0530

    Blank commit

--- app/src/test/java/com/example/ranaranvijaysingh/testingdemo/presenters/MainPresenterTest.java
+++ app/src/test/java/com/example/ranaranvijaysingh/testingdemo/presenters/MainPresenterTest.java
@@ -14,9 +14,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 /**
  * Created by on 14/02/17.
@@ -45,7 +43,6 @@ public class MainPresenterTest {
 
     @Test
     public void presentDataFromApiTest() throws Exception {
-        when(mMockWebService.makeUserListApiCall(any())).thenReturn()
         mMainPresenter.presentDataFromApi();
         verify(mMockMainView).showProgressDialog(true);
         verify(mMockWebService).makeUserListApiCall(mCaptor.capture());
