b6e986d9736cf9f51692f548157fbaeda276857a
Rana Ranvijay SIngh <ranaranvijaysingh@Ranvijay-Mac-mini.local>
2017-05-30 15:25:18 +0530

    Dummy unit test cases added

---
+++ app/src/test/java/com/example/ranaranvijaysingh/testingdemo/presenters/DummyUnitTest.java
@@ -0,0 +1,19 @@
+package com.example.ranaranvijaysingh.testingdemo.presenters;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+/**
+ * Created by on 30/05/17.
+ * Purpose of this class is to
+ */
+
+public class DummyUnitTest {
+
+    @Test
+    public void sum() throws Exception {
+        Assert.assertEquals(4, 2 + 2);
+
+    }
+}
